07590a842d89d9e0385eba1b78b406d960dc3a94
SaliMerc <nyawadesaline@gmail.com>
2025-12-01 20:40:09 +0300

    added the transactions model

--- Admin/models.py
+++ Admin/models.py
@@ -13,3 +13,37 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Transactions(models.Model):
+    """To store payment transactions made by users for products.
+    In real-world applications, the transactions table would be linked to a User model and Products model to know who made a payment to which product. This can be achieved using ForeignKey relationships.
+    Having the transaction method makes your table flexible to accommodate different payment methods.
+    Transaction status helps to track the state of the payment. After a payment is initiated, it should never be pending, but only either completed or failed.
+    """
+    
+    CHOICES_TRANSACTION_STATUS = [
+        ('pending', 'Pending'),
+        ('failed', 'Failed'),
+        ('completed', 'Completed'),
+    ]
+    
+    CHOICES_TRANSACTION_METHOD = [
+        ('mpesa', 'M-PESA'),
+        ('credit_or_debit_card', 'Credit/Debit Card'),
+    ]
+    
+    customer_name = models.CharField(max_length=100)
+    customer_phone_number= models.CharField(max_length=15)
+    
+    transaction_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_method= models.CharField(max_length=30, choices=CHOICES_TRANSACTION_METHOD, default='mpesa')
+    
+    transaction_status= models.CharField(max_length=20, choices=CHOICES_TRANSACTION_STATUS, default='pending')
+    transaction_code = models.CharField(max_length=100, unique=True)
+    transaction_reference_number = models.CharField(max_length=100, unique=True)
+    transaction_result_description = models.TextField(blank=True, null=True)
+    transaction_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transaction {self.transaction_code} - {self.transaction_status}"
